a9ae1597a25870ac8889db54af2e70627a01f5bd
pallaveekumari <pallavee.kumari@razorpay.com>
2023-12-04 00:40:50 +0530

    navbar-second-implemented

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -7,18 +7,33 @@ import {
   FormControl,
   OutlinedInput,
   InputLabel,
-  Avatar
+  Avatar,
+  MenuItem,
+  Menu
 } from "@mui/material";
 import React from "react";
 import styles from "./Navbar.module.css";
 import DehazeOutlinedIcon from "@mui/icons-material/DehazeOutlined";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import SearchIcon from "@mui/icons-material/Search";
-import FolderOutlinedIcon from '@mui/icons-material/FolderOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import FolderOutlinedIcon from "@mui/icons-material/FolderOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 const Navbar = () => {
+
+  
+  const [anchorEl1, setAnchorEl1] = React.useState<null | HTMLElement>(null);
+  const open1 = Boolean(anchorEl1);
+  const handleClick1 = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl1(event.currentTarget);
+  };
+  const handleClose1 = () => {
+    setAnchorEl1(null);
+  }
+
+
+
   return (
     <Box>
       <Box className={styles.catalogNavbar}>
@@ -31,25 +46,35 @@ const Navbar = () => {
             />
           </Box>
           <Box className={styles.catalogBox}>
-            <Typography variant="body2" sx={{fontWeight:"bold"}} className={styles.catalogText}>
+            <Typography
+              variant="body2"
+              sx={{ fontWeight: "bold" }}
+              className={styles.catalogText}
+            >
               Catalog
             </Typography>
-            <Typography variant="body2" sx={{fontWeight:"bold"}}>BuyDesk</Typography>
+            <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+              BuyDesk
+            </Typography>
             <Box className={styles.historyDropdown}>
-              <Typography variant="body2" sx={{fontWeight:"bold"}}>History </Typography>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                History{" "}
+              </Typography>
               <ArrowDropDownIcon />
             </Box>
-            <Typography variant="body2" sx={{fontWeight:"bold"}}>Dashboard</Typography>
+            <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+              Dashboard
+            </Typography>
           </Box>
         </Box>
 
         <Box className={styles.poBox}>
           <Box className={styles.historyDropdownMenu}>
-            <Box >PO</Box>
+            <Box>PO</Box>
             <ArrowDropDownIcon />
           </Box>
           <Box>
-            <FormControl sx={{width: "25ch" }} variant="outlined">
+            <FormControl sx={{ width: "25ch" }} variant="outlined">
               <OutlinedInput
                 id="outlined-adornment-password"
                 size="small"
@@ -66,17 +91,76 @@ const Navbar = () => {
           </Box>
           <Box className={styles.help}>Help</Box>
           <Box className={styles.IconBoxes}>
-          <FolderOutlinedIcon/>
-          <FavoriteBorderOutlinedIcon/>
-          <ShoppingCartOutlinedIcon/>
-          <NotificationsNoneOutlinedIcon/>
+            <FolderOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <ShoppingCartOutlinedIcon />
+            <NotificationsNoneOutlinedIcon />
           </Box>
           <Box className={styles.avtar}>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-          <Box>Sara Green</Box>
-          <ArrowDropDownIcon />
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            <Box>Sara Green</Box>
+            <ArrowDropDownIcon />
           </Box>
         </Box>
+      </Box>
+
+      <Box className={styles.secondNavbar}>
+        <Box className={styles.secondNavbarBox1}>
+          <Box className={styles.purchase}>
+            <Box>Purchase organization</Box>
+            <ArrowDropDownIcon />
+          </Box>
+          <Box className={styles.company}>
+            <Box>Company</Box>
+            <ArrowDropDownIcon />
+          </Box>
+          <Box className={styles.plant}>
+            <Box>Plant</Box>
+            <ArrowDropDownIcon />
+          </Box>
+        </Box>
+
+        <Box className={styles.secondNavbarBox2}>
+          <OutlinedInput className={styles.secondNavbarSearchBox} placeholder="Search" size="small" />
+          <Box className={styles.searchiconsbox}>
+          <SearchIcon/>
+
+        
+
+          </Box>
+          <Box className={styles.upload}>Upload</Box>
+
+          <Box className={styles.filter} onClick={handleClick1}>
+            <Box>Filter</Box>
+            <ArrowDropDownIcon   />
+            <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl1}
+        open={open1}
+        onClose={handleClose1}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem onClick={handleClose1}>Profile</MenuItem>
+        <MenuItem onClick={handleClose1}>My account</MenuItem>
+        <MenuItem onClick={handleClose1}>Logout</MenuItem>
+      </Menu>
+          </Box>
+
+          <Box className={styles.sortby}>
+            <Box>Sort By</Box>
+            <ArrowDropDownIcon />
+           
+          </Box>
+        </Box>
+        <Box className={styles.secondNavbarBox3}></Box>
       </Box>
     </Box>
   );

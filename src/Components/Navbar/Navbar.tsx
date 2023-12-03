4e22e89216b5b17d8ac7f2b73ebc60945e068fe3
pallaveekumari <pallavee.kumari@razorpay.com>
2023-12-03 23:17:29 +0530

    navbar_created

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,8 +1,23 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Typography,
+  TextField,
+  InputAdornment,
+  IconButton,
+  FormControl,
+  OutlinedInput,
+  InputLabel,
+  Avatar
+} from "@mui/material";
 import React from "react";
 import styles from "./Navbar.module.css";
 import DehazeOutlinedIcon from "@mui/icons-material/DehazeOutlined";
-
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import SearchIcon from "@mui/icons-material/Search";
+import FolderOutlinedIcon from '@mui/icons-material/FolderOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 const Navbar = () => {
   return (
     <Box>
@@ -16,10 +31,50 @@ const Navbar = () => {
             />
           </Box>
           <Box className={styles.catalogBox}>
-          <Typography variant="body2" className={styles.catalogText}>Catalog</Typography>
-          <Typography variant="body2">BuyDesk</Typography>
-          <Typography variant="body2">History</Typography>
-          <Typography variant="body2">Dashboard</Typography>
+            <Typography variant="body2" sx={{fontWeight:"bold"}} className={styles.catalogText}>
+              Catalog
+            </Typography>
+            <Typography variant="body2" sx={{fontWeight:"bold"}}>BuyDesk</Typography>
+            <Box className={styles.historyDropdown}>
+              <Typography variant="body2" sx={{fontWeight:"bold"}}>History </Typography>
+              <ArrowDropDownIcon />
+            </Box>
+            <Typography variant="body2" sx={{fontWeight:"bold"}}>Dashboard</Typography>
+          </Box>
+        </Box>
+
+        <Box className={styles.poBox}>
+          <Box className={styles.historyDropdownMenu}>
+            <Box >PO</Box>
+            <ArrowDropDownIcon />
+          </Box>
+          <Box>
+            <FormControl sx={{width: "25ch" }} variant="outlined">
+              <OutlinedInput
+                id="outlined-adornment-password"
+                size="small"
+                placeholder="Search"
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton edge="end">
+                      <SearchIcon />
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </Box>
+          <Box className={styles.help}>Help</Box>
+          <Box className={styles.IconBoxes}>
+          <FolderOutlinedIcon/>
+          <FavoriteBorderOutlinedIcon/>
+          <ShoppingCartOutlinedIcon/>
+          <NotificationsNoneOutlinedIcon/>
+          </Box>
+          <Box className={styles.avtar}>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          <Box>Sara Green</Box>
+          <ArrowDropDownIcon />
           </Box>
         </Box>
       </Box>
